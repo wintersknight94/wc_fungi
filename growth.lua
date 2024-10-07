f2bd0f50fe7a04101c30733eed4fcb02e538a4fa
Wyntr Stardust <62315647+wintersknight94@users.noreply.github.com>
2024-10-06 20:08:18 -0500

    fixed orientation bug, nerfed growth rate

--- growth.lua
+++ growth.lua
@@ -29,7 +29,7 @@ local function shroomgrowth(id, maxlight)
 			if light >= maxlight then return
 			end
 				if nodecore.air_equivalent(anode) then
-					nodecore.set_loud(pos, {name = modname.. ":mushroom_tall" ..id})
+					nodecore.set_loud(pos, {name = modname.. ":mushroom_tall" ..id, param2 = 10})
 					nodecore.witness(pos, "tallshroom growth")
 				end
 		end
@@ -46,27 +46,24 @@ minetest.register_abm({
 	label = "Shroom Growth",
 --	nodenames = {groups = {fungal = true}},
 	nodenames = {"group:mycelium"},
-	interval = 120, --120
-	chance = 20, --20
+	interval = 600,
+	chance = 20,
 	action = function(pos)
 		local above = {x = pos.x, y = pos.y + 1, z = pos.z}
---		minetest.chat_send_all("action works")
 		if not nodecore.buildable_to(above) then
---			minetest.chat_send_all("blocked above")
 			return
 		end
 		local light = nodecore.get_node_light(above)
 		if (not light) or light >= 8 then
---			minetest.chat_send_all("wrong light")
 			return
 		end
 		if light >= 4 then
-			return nodecore.set_loud(above, {name = modname .. ":mushroom"})
+			return nodecore.set_loud(above, {name = modname .. ":mushroom", param2 = 10})
 		end
 		if #nodecore.find_nodes_around(pos, "group:lux_emit", 2) > 0 then
-			return nodecore.set_loud(above, {name = modname .. ":mushroom_lux"})
+			return nodecore.set_loud(above, {name = modname .. ":mushroom_lux", param2 = 10})
 		end
-		return nodecore.set_loud(above, {name = modname .. ":mushroom_glow"})
+		return nodecore.set_loud(above, {name = modname .. ":mushroom_glow", param2 = 10})
 	end
 })
 
@@ -75,8 +72,8 @@ minetest.register_abm({
 	label = "Mushroom Growth",
 	nodenames = {"nc_tree:humus"},
 	neighbors = {"group:moist"},
-	interval = 900, --900
-	chance = 100, --100
+	interval = 900,
+	chance = 100,
 	action = function(pos)
 		local above = {x = pos.x, y = pos.y + 1, z = pos.z}
 --		minetest.chat_send_all("action works")
@@ -89,7 +86,7 @@ minetest.register_abm({
 --			minetest.chat_send_all("wrong light")
 			return
 		end
-		return nodecore.set_loud(above, {name = modname .. ":mushroom"})
+		return nodecore.set_loud(above, {name = modname .. ":mushroom", param2 = 10})
 	end
 })
 -- ================================================================== --
@@ -112,9 +109,9 @@ nodecore.register_abm({
 			return nodecore.set_loud(next_pos, {name = modname .. ":mushroom_shelf", param2 = minetest.dir_to_wallmounted(vector.multiply(dir, -1))})
 		end
 		if #nodecore.find_nodes_around(pos, "group:lux_emit", 2) > 0 then
-			return nodecore.set_loud(next_pos, {name = modname .. ":mushroom_shelf_lux"})
+			return nodecore.set_loud(next_pos, {name = modname .. ":mushroom_shelf_lux", param2 = minetest.dir_to_wallmounted(vector.multiply(dir, -1))})
 		end
-		return nodecore.set_loud(next_pos, {name = modname .. ":mushroom_shelf_glow"})
+		return nodecore.set_loud(next_pos, {name = modname .. ":mushroom_shelf_glow", param2 = minetest.dir_to_wallmounted(vector.multiply(dir, -1))})
 		end
 	end
 })
