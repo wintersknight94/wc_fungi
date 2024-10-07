48173e5207a9593dd53422d8a65d8b633f5bd336
Wyntr Stardust <62315647+wintersknight94@users.noreply.github.com>
2024-10-06 20:07:16 -0500

    new giant honeycap schematic

--- schematics.lua
+++ schematics.lua
@@ -286,3 +286,98 @@ nodecore.bigmushroom_lux_schematic = nodecore.ezschematic(
 		}
 	}
 )
+
+-- <>==============================================================<> --
+
+local honeystem	= modname.. ":mushroom_stalk_honey"
+local honeycap	= modname.. ":mushroom_cap_honey"
+local honeygills	= modname.. ":mushroom_gills_honey"
+
+local root = {
+	".......",
+	"..rrr..",
+	".rrRrr.",
+	".rRRRr.",
+	".rrRrr.",
+	"..rrr..",
+	".......",
+}
+local stem = {
+	".......",
+	".......",
+	".......",
+	"...t...",
+	".......",
+	".......",
+	".......",
+}
+local caplow = {
+	"..ccc..",
+	".cgggc.",
+	"cgggggc",
+	"cggtggc",
+	"cgggggc",
+	".cgggc.",
+	"..ccc..",
+}
+local capmid = {
+	".......",
+	"..ccc..",
+	".ccccc.",
+	".ccccc.",
+	".ccccc.",
+	"..ccc..",
+	".......",
+}
+local caphi = {
+	".......",
+	".......",
+	"..ccc..",
+	"..ccc..",
+	"..ccc..",
+	".......",
+	".......",
+}
+nodecore.bigmushroom_honey_schematic = nodecore.ezschematic(
+	{
+		["."] = {name = "air", prob = 0},
+		R = {name = mycelium2, prob = 255, force_place = true},
+		r = {name = mycelium1, prob = 100, force_place = true},
+		t = {name = honeystem, prob = 255, force_place = true},
+		c = {name = honeycap, prob = 255},
+		g = {name = honeygills, prob = 255},
+	},
+	{
+		root,
+		root,
+		stem,
+		stem,
+		stem,
+		stem,
+		stem,
+		stem,
+		stem,
+		stem,
+		caplow,
+		capmid,
+		caphi
+	},
+	{
+		yslice_prob = {
+			{ypos = 1, prob = 255},
+			{ypos = 2, prob = 255},
+			{ypos = 3, prob = 255},
+			{ypos = 4, prob = 100},
+			{ypos = 5, prob = 100},
+			{ypos = 6, prob = 100},
+			{ypos = 7, prob = 100},
+			{ypos = 8, prob = 100},
+			{ypos = 9, prob = 100},
+			{ypos = 10, prob = 255},
+			{ypos = 11, prob = 255},
+			{ypos = 12, prob = 255},
+			{ypos = 13, prob = 255},
+		}
+	}
+)
+-- <>==============================================================<> --
